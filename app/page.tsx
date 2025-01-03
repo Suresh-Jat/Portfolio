b6ffeba3a0a294f5442fff4d757a18861ca4b32c
Suresh Jat <sureshjat@Sureshs-Laptop.local>
2025-01-03 17:26:20 +0530

    Experiences and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Experiences from "@/components/Experiences";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -6,7 +8,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
 
       <div className="max-w-7xl w-full">
         <FloatingNav
@@ -15,6 +17,8 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Experiences />
+        <Footer />
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
+    { name: "Expriences", link: "#Expriences" },
     { name: "Contact", link: "#contact" },
   ];
   
